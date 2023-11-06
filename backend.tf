cd6a065274250409adee8585c3c576486b1205c1
kjha4951 <kjha4951@gmail.com>
2023-11-06 12:13:51 +0530

    newrelic

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 
 terraform {
   backend "s3" {
-    bucket         = "ewrelicpractice"
+    bucket         = "newrelicpractice"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    encrypt        = true
-    
+
+
   }
 }
